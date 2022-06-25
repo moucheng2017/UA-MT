9064f9c18263e22e0fbcf48bc89de95748debe0d
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-06-25 17:37:23 +0100

    2022 06 25: debugging for mismatch

---
+++ base.sh
@@ -0,0 +1,8 @@
+#$ -l tmem=16G
+#$ -l gpu=true
+#$ -S /bin/bash
+#$ -j y
+#$ -l h_rt=24:00:00
+#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_airway
+
+~/miniconda3/envs/pytorch1.4/bin/python train_LA_meanteacher_certainty_unlabel.py
