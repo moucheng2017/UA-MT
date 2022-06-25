a526ce6087f7313504f9c98550c9db67af79159c
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-06-25 17:45:34 +0100

    2022 06 25b: modifying the path for exps on cluster

--- mismatch.sh
+++ mismatch.sh
@@ -3,6 +3,6 @@
 #$ -S /bin/bash
 #$ -j y
 #$ -l h_rt=24:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/exps_airway
+#$ -wd /SAN/medic/PerceptronHead/codes/UA-MT/code
 
 ~/miniconda3/envs/pytorch1.4/bin/python train_LA_meanteacher_certainty_unlabel_mismatch.py
