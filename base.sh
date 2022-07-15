32879fdd8bbedef8221425ae752366af95ddd1f8
moucheng2017 <rmapmxu@ucl.ac.uk>
2022-07-15 13:56:36 +0100

    2022 07 15 a: adding training of two more experiments

--- base.sh
+++ base.sh
@@ -1,8 +1,8 @@
-#$ -l tmem=16G
+#$ -l tmem=12G
 #$ -l gpu=true
 #$ -S /bin/bash
 #$ -j y
 #$ -l h_rt=24:00:00
 #$ -wd /SAN/medic/PerceptronHead/codes/UA-MT/code
 
-~/miniconda3/envs/pytorch1.4/bin/python train_LA_meanteacher_certainty_unlabel.py --consistency 1.0 --labels 2
+~/miniconda3/envs/pytorch1.4/bin/python train_LA_meanteacher_certainty_unlabel.py --consistency 1.0 --labels 1
--- mismatch.sh
+++
@@ -1,8 +0,0 @@
-#$ -l tmem=16G
-#$ -l gpu=true,gpu_type=(rtx6000|rtx8000)
-#$ -S /bin/bash
-#$ -j y
-#$ -l h_rt=24:00:00
-#$ -wd /SAN/medic/PerceptronHead/codes/UA-MT/code
-
-~/miniconda3/envs/pytorch1.4/bin/python train_LA_meanteacher_certainty_unlabel_mismatch.py --consistency 1.0 --labels 2
